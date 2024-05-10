9415e91290f1bc2741df892b1bacfd00ed846048
bogdanius00102 <64437264+bogdanius00102@users.noreply.github.com>
2024-05-10 12:02:53 +0300

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,12 @@ function btn_click()
 
 let txt1=document.getElementById("txt1").value;
 let txt2=document.getElementById("txt2").value;
-let array=[txt1,txt2];tg.sendData(array);
+
+let array = [txt1, txt2];
+let jsonString = JSON.stringify(array);
+
+
+tg.sendData(jsonString);
 
 }
 
