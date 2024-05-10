07cd83fa26a6f16f2c0bdc200ac962b8c6fbbf6a
bogdanius00102 <64437264+bogdanius00102@users.noreply.github.com>
2024-05-10 15:44:26 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let tg = window.Telegram.WebApp;
 
 tg.expand();
 var mb=tg.MainButton;
-mb.text="Відправити2";
+mb.text="Надіслати";
 
 
 
@@ -14,8 +14,9 @@ function send_form()
 
 let txt1=document.getElementById("txt1").value;
 let txt2=document.getElementById("txt2").value;
+let txt3=document.getElementById("txt3").value;
 
-if(txt1.length<3 || txt2.length<3)
+if(txt1.length<3 || txt2.length<3|| txt3.length<3)
 {
 
 
@@ -35,7 +36,8 @@ function sendData()
 {
 let txt1=document.getElementById("txt1").value;
 let txt2=document.getElementById("txt2").value;
-let array = [txt1, txt2];
+let txt3=document.getElementById("txt3").value;
+let array = [txt1, txt2,txt3];
 
 tg.sendData(array);
 
