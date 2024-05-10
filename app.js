4b74c40a762202c40577e32f0f5059c221ed567b
bogdanius00102 <64437264+bogdanius00102@users.noreply.github.com>
2024-05-10 11:22:43 +0300

    Update app.js

--- app.js
+++ app.js
@@ -39,8 +39,7 @@ let usercard = document.getElementById("usercard");
 
 let p = document.createElement("p");
 
-p.innerText = `${tg.initDataUnsafe.user.first_name}
-${tg.initDataUnsafe.user.last_name}`;
+p.innerText = tg.initDataUnsafe.user.first_name;
 
 usercard.appendChild(p);
 function btn_click()
