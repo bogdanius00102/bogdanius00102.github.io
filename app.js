273c567a476a93be92ac93d529a40e8f99de3920
bogdanius00102 <64437264+bogdanius00102@users.noreply.github.com>
2024-05-10 12:17:10 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,26 +1,55 @@
 let tg = window.Telegram.WebApp;
 
 tg.expand();
+var mb=tg.MainButton;
+mb.text="Відправити";
+mb.textColor = '#FFFFFF';
+mb.color = '#2cab37';
+
+
+
 
 let usercard = document.getElementById("usercard").innerText = tg.initDataUnsafe.user.first_name;
 
 
-function btn_click()
+function send_form()
 {
 
 let txt1=document.getElementById("txt1").value;
 let txt2=document.getElementById("txt2").value;
+console.log(txt2);
+if(txt1.length<3 || txt2.length<3)
+{
 
-let array = [txt1, txt2];
-let jsonString = JSON.stringify(array);
 
-
-tg.sendData(jsonString);
+	mb.hide();
+}
+else
+{
+	mb.show();
+}
 
 }
 
 
 
 
+function sendData()
+{
+let txt1=document.getElementById("txt1").value;
+let txt2=document.getElementById("txt2").value;
+let array = [txt1, txt2];
+if(txt1.length<3 || txt2.length<3)
+{
+	return;
+}
+else
+{
+tg.sendData(array);
+}
 
+}
 
+Telegram.WebApp.onEvent("mainButtonClicked", function(){
+	sendData();
+});
